6edb13f1fe00cab5bb1fb7ee89a0f06638f25a34
nhatnguyen <nhatnguyen1026@gmail.com>
2025-03-26 15:00:19 +0700

    rebuild again

--- web-mobile/index.3e09f.js
+++ web-mobile/index.3e09f.js
@@ -1,4 +1,4 @@
-System.register(["./application.cba18.js"], function (_export, _context) {
+System.register(["./application.653fe.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
