cdb82920d2839ec64dbe318f6930032a8600af77
nhatnguyen <nhatnguyen1026@gmail.com>
2025-03-27 17:33:19 +0700

    update

--- web-mobile/index.b1dde.js
+++ web-mobile/index.b1dde.js
@@ -1,4 +1,4 @@
-System.register(["./application.1ef3c.js"], function (_export, _context) {
+System.register(["./application.24a5f.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
