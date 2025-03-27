b1f05632544b995c6a123967073eff616be74c96
nhatnguyen <nhatnguyen1026@gmail.com>
2025-03-27 08:39:59 +0700

    update test

--- web-mobile/index.b75c2.js
+++ web-mobile/index.b75c2.js
@@ -1,4 +1,4 @@
-System.register(["./application.9024d.js"], function (_export, _context) {
+System.register(["./application.5b836.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
