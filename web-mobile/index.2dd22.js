64823a9929e55572b83b61b619bbfe519e121b78
nhatnguyen <nhatnguyen1026@gmail.com>
2025-03-28 08:27:49 +0700

    update

--- web-mobile/index.2dd22.js
+++ web-mobile/index.2dd22.js
@@ -1,4 +1,4 @@
-System.register(["./application.24a5f.js"], function (_export, _context) {
+System.register(["./application.81011.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
