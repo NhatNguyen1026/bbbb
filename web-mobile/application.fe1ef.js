51f4cf8c3975f339e9a8c0c04309421bc048109c
nhatnguyen <nhatnguyen1026@gmail.com>
2025-03-31 11:43:59 +0700

    test time load

--- web-mobile/application.fe1ef.js
+++ web-mobile/application.fe1ef.js
@@ -8,7 +8,7 @@ System.register([], function (_export, _context) {
     execute: function () {
       _export("Application", Application = class Application {
         constructor() {
-          this.settingsPath = 'src/settings.8b27d.json';
+          this.settingsPath = 'src/settings.697b7.json';
           this.showFPS = false;
         }
         init(engine) {
--- web-mobile/index.bd2df.js
+++ web-mobile/index.bd2df.js
@@ -1,4 +1,4 @@
-System.register(["./application.681d7.js"], function (_export, _context) {
+System.register(["./application.fe1ef.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
