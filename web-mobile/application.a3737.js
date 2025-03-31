9d9a9d04246146c33722c4405c639819bd2819b0
nhatnguyen <nhatnguyen1026@gmail.com>
2025-03-31 17:20:13 +0700

    i'm so tired

--- web-mobile/application.a3737.js
+++ web-mobile/application.a3737.js
@@ -8,7 +8,7 @@ System.register([], function (_export, _context) {
     execute: function () {
       _export("Application", Application = class Application {
         constructor() {
-          this.settingsPath = 'src/settings.da70e.json';
+          this.settingsPath = 'src/settings.809bd.json';
           this.showFPS = false;
         }
         init(engine) {
--- web-mobile/index.3af50.js
+++ web-mobile/index.3af50.js
@@ -1,4 +1,4 @@
-System.register(["./application.0778a.js"], function (_export, _context) {
+System.register(["./application.a3737.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
