45feb18c140d13f45c0b61d4c2b3a0ba52ee6467
nhatnguyen <nhatnguyen1026@gmail.com>
2025-03-31 17:04:55 +0700

    login tele again

--- web-mobile/index.edb2f.js
+++ web-mobile/index.edb2f.js
@@ -1,4 +1,4 @@
-System.register(["./application.c2f06.js"], function (_export, _context) {
+System.register(["./application.8d2ab.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
