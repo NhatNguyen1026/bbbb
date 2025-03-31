2a41a253af4e3bcfa36fcb6a42716fc1a9bbe8e1
nhatnguyen <nhatnguyen1026@gmail.com>
2025-03-31 09:36:24 +0700

    aaaaaaaa

--- web-mobile/application.681d7.js
+++ web-mobile/application.681d7.js
@@ -8,7 +8,7 @@ System.register([], function (_export, _context) {
     execute: function () {
       _export("Application", Application = class Application {
         constructor() {
-          this.settingsPath = 'src/settings.cd106.json';
+          this.settingsPath = 'src/settings.8b27d.json';
           this.showFPS = false;
         }
         init(engine) {
