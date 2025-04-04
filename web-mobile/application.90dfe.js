f738e701314cd1f47591e6a4ffa2678d42a6d50e
nhatnguyen <nhatnguyen1026@gmail.com>
2025-04-04 14:24:45 +0700

    going for a 15th billionth time

--- web-mobile/application.90dfe.js
+++ web-mobile/application.90dfe.js
@@ -8,7 +8,7 @@ System.register([], function (_export, _context) {
     execute: function () {
       _export("Application", Application = class Application {
         constructor() {
-          this.settingsPath = 'src/settings.085f2.json';
+          this.settingsPath = 'src/settings.d0040.json';
           this.showFPS = false;
         }
         init(engine) {
