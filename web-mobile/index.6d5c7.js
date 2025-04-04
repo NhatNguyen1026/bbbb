fde5e9b9a3082b2dffa9f7a9b2b4f3ef94090517
nhatnguyen <nhatnguyen1026@gmail.com>
2025-04-04 16:53:19 +0700

    update again

--- web-mobile/index.6d5c7.js
+++ web-mobile/index.6d5c7.js
@@ -1,4 +1,4 @@
-System.register(["./application.e1613.js"], function (_export, _context) {
+System.register(["./application.aef9d.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
