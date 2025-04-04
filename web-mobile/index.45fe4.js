d20f381827843f47f0b46ff0780dd0790dd5ed1d
nhatnguyen <nhatnguyen1026@gmail.com>
2025-04-04 15:54:23 +0700

    well this could be it maybe

--- web-mobile/index.45fe4.js
+++ web-mobile/index.45fe4.js
@@ -1,4 +1,4 @@
-System.register(["./application.9fbd3.js"], function (_export, _context) {
+System.register(["./application.e1613.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
