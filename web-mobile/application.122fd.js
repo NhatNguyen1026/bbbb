53c98cdb648fe6a60c94cce06405097824a5228b
nhatnguyen <nhatnguyen1026@gmail.com>
2025-04-10 14:05:18 +0700

    testbuild

--- web-mobile/application.122fd.js
+++ web-mobile/application.122fd.js
@@ -8,7 +8,7 @@ System.register([], function (_export, _context) {
     execute: function () {
       _export("Application", Application = class Application {
         constructor() {
-          this.settingsPath = 'src/settings.0b7c5.json';
+          this.settingsPath = 'src/settings.b25e5.json';
           this.showFPS = false;
         }
         init(engine) {
